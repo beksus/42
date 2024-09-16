25a282b89daab33f95d7bfd7517fc35629fc5349
Beks <85835904+beksus@users.noreply.github.com>
2024-09-17 01:27:51 +0600

    Create ft_putstr.c

---
+++ ft_putstr.c
@@ -0,0 +1,27 @@
+/*Assignment name  : ft_putstr
+Expected files   : ft_putstr.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a function that displays a string on the standard output.
+
+The pointer passed to the function contains the address of the string's first
+character.
+
+Your function must be declared as follows:
+
+void	ft_putstr(char *str);
+*/
+
+
+void	ft_putstr(char *str)
+{
+    int count = 0;
+    
+    while(str[count] != '\0')
+    {
+        write(1, &str[count], 1);
+        count++;
+    }
+    write(1, "\n", 1);
+}
